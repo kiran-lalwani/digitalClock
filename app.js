f5780fb60edd0e98665fc6c94d1924be0d63e8d4
kiran-lalwani <kiranlalwani2702@gmail.com>
2026-03-14 23:43:49 +0530

    Remove commented-out updateTime function
    Removed commented-out code for the updateTime function and related logic.

--- app.js
+++ app.js
@@ -30,36 +30,8 @@ let btnMinus = document.querySelector("#btn-minus");
    
  
 
-// function updateTime() {
-// const now =  new Date();
-
-// let day = now.getDate();
-// let month = now.getMonth() + 1;
-// let  year = now.getFullYear();
-
-// let hour = now.getHours();
-// let min = now.getMinutes();
-// let sec = now.getSeconds();
-
-// // if(day < 10) day = "0" + day;
-// // if(month < 10) month = "0" + month;
-// // if(hour < 10) hour = "0" + hour;
-// // if(min < 10) min = "0" + min;
-// // if(sec < 10) sec = "0" + sec;
-// day = day < 10 ? '0' + day : day;
-// month = month < 10 ? '0' + month : month;
-// hour = hour < 10 ? '0' + hour : hour;
-// min = min < 10 ? '0' + min : min;
-// sec = sec < 10 ? '0' + sec : sec;
 
 
-// time.textContent = `Date : ${day}/${month}/${year} 
-//                     Time : ${hour}:${min}:${sec}`;
-
-// };
-// //updateTime();
-// setInterval(updateTime, 1000);
-//👉 Digital Clock → Counter App → To-Do List → Quiz App
  
 
 
